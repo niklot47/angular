63c6e5681e32d399696b047fc9e361f475a156d1
MykolaKozakov <niklot47@gmail.com>
2022-05-13 19:14:16 +0300

    hw1.2

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,23 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+import {IUser} from "../../interfaces";
+
+// @ts-ignore
+// @ts-ignore
+// @ts-ignore
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  @Input()
+  user:IUser
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
