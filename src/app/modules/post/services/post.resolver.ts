09ef4785c8a59dea939827a2540d630c8eb00370
MykolaKozakov <niklot47@gmail.com>
2022-05-24 15:56:43 +0300

    hw4

---
+++ src/app/modules/post/services/post.resolver.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  Router, Resolve,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot
+} from '@angular/router';
+import { Observable, of } from 'rxjs';
+
+import {IPost} from "../interfaces";
+import {PostService} from "./post.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class PostResolver implements Resolve<IPost> {
+
+  constructor(private postService:PostService) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IPost> | Promise<IPost> | IPost {
+    const {id} = route.params;
+    return this.postService.getById(id);
+  }
+}
---
+++ src/app/modules/todos/services/todos.guard.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, CanDeactivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodosGuard implements CanDeactivate<boolean> {
+ canDeactivate(component: boolean, currentRoute: ActivatedRouteSnapshot, currentState: RouterStateSnapshot, nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+  return confirm("Leave todos list?");
+ }
+
+}
